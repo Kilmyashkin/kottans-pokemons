1de4deb9c5c64188b5794a5607a706b41e126ac5
spock <akilmyashkin@gmail.com>
2016-04-10 21:14:26 +0300

    Adding pages

---
+++ pokemon.js
@@ -0,0 +1,104 @@
+
+var pokedexApp = angular.module('pokedexApp',[]);
+
+pokedexApp.controller('PokedexCtrl', ['$scope', '$http',
+    function ($scope, $http){
+      $('.pokedex__preloader').addClass("pokedex__preloader--active");
+      $('.pokedex').addClass("pokedex--hide-filter pokedex--hide-pokemons");
+      $http.get('http://pokeapi.co/api/v1/type/?limit=999').success(function(data){
+          $scope.pokemonTypes = data;
+          $('.pokedex').removeClass("pokedex--hide-pokemons");
+      });
+      $http.get('http://pokeapi.co/api/v1/pokemon/?limit=12').success(function(data){
+          $scope.pokemons = data;
+          $('.pokedex__preloader').removeClass("pokedex__preloader--active");
+          $('.pokedex').removeClass("pokedex--hide-filter");
+      });
+
+
+    $scope.getInfo = function(id){
+      $http.get('http://pokeapi.co/api/v1/pokemon/' + id).success(function(data){
+          $scope.pokemonInfo = data;
+      });
+      $('.pokemon-details').css('display', 'inline-block');
+      $('#pokemonPic').attr('src', 'http://pokeapi.co/media/img/'+ id +'.png');
+    };
+
+    $scope.loadMorePokemons = function(url){
+      $('.btn-more__spinner').addClass("btn-more__spinner--active");
+      $('.btn-more').addClass("btn-more--inactive");
+      $http.get('http://pokeapi.co/' + url).success(function(data){
+          $scope.pokemons.meta = data.meta;
+          $scope.pokemons.objects = $scope.pokemons.objects.concat(data.objects);
+          $('.btn-more__spinner').removeClass("btn-more__spinner--active");
+          $('.btn-more').removeClass("btn-more--inactive");
+      });
+
+      $scope.loadMoreButton = true;
+      $scope.displayPokemons($scope.filterChecked);
+    };
+
+    $scope.filterChecked = [];
+
+    $scope.removeInitState = function() {
+      var filterObj = $scope.pokemons.objects;
+        for (var pokemon in filterObj) {
+          filterObj[pokemon].checked = false;
+        }
+    };
+
+    $scope.returnInitState = function() {
+      var filterObj = $scope.pokemons.objects;
+      for (var pokemon in filterObj) {
+        filterObj[pokemon].checked = true;
+      }
+    };
+
+    $scope.checkAll = function () {
+        $scope.loadMoreButton = true;
+        $scope.filterChecked = [];
+
+        $scope.selectedAll = false;
+        $scope.returnInitState();
+        angular.forEach($scope.pokemonTypes.objects , function (type) {
+            type.selected = $scope.selectedAll;
+        });
+
+        $scope.displayPokemons($scope.filterChecked);
+        $scope.loadMoreButton = true;
+    };
+
+    $scope.includeTypes = function(type) {
+      var name = type.name.toLowerCase();
+      var index = $scope.filterChecked.indexOf(name);
+      if (type.selected) {
+        $scope.filterChecked.push(name);
+      } else {
+        $scope.filterChecked.splice(index, 1);
+      }
+
+      $scope.displayPokemons($scope.filterChecked);
+    };
+
+    $scope.displayPokemons = function(checked) {
+      $scope.loadMoreButton = false;
+      var filterObj = $scope.pokemons.objects;
+      for (var pokemon in filterObj) {
+        var pokTypes = filterObj[pokemon].types;
+        for (var poktype in pokTypes) {
+          var index = checked.indexOf(filterObj[pokemon].types[poktype].name);
+          if (index !== -1) {
+            filterObj[pokemon].checked = true;
+            }
+          }
+        }
+
+        if ($scope.filterChecked.length === 0) {
+          $scope.returnInitState();
+          $scope.loadMoreButton = true;
+        } else if ($scope.filterChecked.length === 20) {
+          $scope.loadMoreButton = true;
+        }
+      };
+
+   }]);
